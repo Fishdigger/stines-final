e9cd6088ab8d9967851a133607e47f63b2c90587
Joel Gregory <joelgregory7@gmail.com>
2017-04-28 23:20:23 -0400

    Created account registration page.

---
+++ register.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+<head>
+  <?php $root = realpath($_SERVER["DOCUMENT_ROOT"]); ?>
+  <?php
+    $page_title = "Register Account";
+    $page_description = "Login to this amazing app.";
+    include("$root/final/templates/head.php");
+  ?>
+</head>
+<body>
+
+  <?php include("$root/final/templates/header.php"); ?>
+
+  <main class="container-fluid">
+
+    <h1 class="text-center display-3 page-title">Create New Account</h1>
+
+    <div class="container-fluid">
+      <?php if(!isset($_POST['register'])){?>
+      <form name="new_account" method="POST">
+        <div class="form-group">
+          <input type="text" class="form-control" name="email" id="email"
+          placeholder="Email Address">
+          <input type="password" class="form-control" name="password" id="password"
+          placeholder="Password">
+          <input type="password" id="conf_password" class="form-control"
+          placeholder="Confirm Password">
+        </div>
+        <div class="form-group">
+          <input type="text" name="first_name" id="first_name" class="form-control"
+          placeholder="First Name">
+          <input type="text" name="last_name" id="last_name" class="form-control"
+          placeholder="Last Name">
+          <input type="text" name="phone" id="phone" class="form-control"
+          placeholder="Phone ex: 5551234567">
+        </div>
+        <div class="form-check form-group">
+          <label class="form-check-label">
+            <input type="radio" name="gender" id="gender_m" value="M">
+            Male
+          </label>
+          <label class="form-check-label">
+            <input type="radio" name="gender" id="gender_f" value="F">
+            Female
+          </label>
+        </div>
+        <div class="form-group">
+          <input type="text" name="address" id="address" class="form-control"
+          placeholder="Street Address">
+          <input type="text" name="city" id="city" class="form-control"
+          placeholder="City">
+          <input type="text" name="state" id="state" class="form-control"
+          placeholder="State">
+          <input type="text" name="zip" id="zip" class="form-control"
+          placeholder="Zip Code">
+        </div>
+        <input type="submit" class="btn btn-primary form-control" name="register"
+        id="register" value="Register New Account">
+      </form>
+
+    </div>
+
+    <?php }
+      if(isset($_POST['register'])){
+        require("$root/final/infrastructure/data_objects/customer_do.php");
+        require("$root/final/infrastructure/data_objects/user_do.php");
+        $custDTO = new Customer_DO();
+        $usDTO = new User_DO();
+        $good = $usDTO->create(array($_POST['email'], $_POST['password']));
+        if($good["status"] == True){
+          $custArray = array(
+            "id" => $good["id"],
+            "email" => $_POST["email"],
+            "first_name" => $_POST["first_name"],
+            "last_name" => $_POST["last_name"],
+            "address" => $_POST["address"],
+            "city" => $_POST["city"],
+            "state" => $_POST["state"],
+            "zip" => $_POST["zip"],
+            "phone" => $_POST["phone"],
+            "gender" => $_POST["gender"]
+          );
+          $still_good = $custDTO->create($custArray);
+          if($still_good){
+            header("Location: ./account.php?id=".$good["id"]);
+          }
+        }
+      }
+     ?>
+
+  </main>
+
+</body>
+<?php include("$root/final/templates/footer.php"); ?>
+</html>
